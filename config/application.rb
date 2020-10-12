ecb6cc3be099f98bacef1a5bea3014b614111f60
David Chmura <dschmura@humbledaisy.com>
2020-10-12 11:01:31 -0400

    ADD: ActiveStorage

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,21 @@ module MiClassrooms
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.generators do |g|
+      g.test_framework false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.channel assets: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators.system_tests = nil
   end
+  ActiveStorage::Engine.config.active_storage.content_types_to_serve_as_binary.delete("image/svg+xml")
+
+  ActiveStorage::Engine.config.active_storage.content_types_allowed_inline.append("image/svg+xml")
+
 end
