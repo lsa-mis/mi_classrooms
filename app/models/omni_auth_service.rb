8712dc6cb1861fc3e935677f60900a95e55f7132
David Chmura <dschmura@humbledaisy.com>
2020-10-12 10:23:16 -0400

    UPDATE: ran annotate gem

--- app/models/omni_auth_service.rb
+++ app/models/omni_auth_service.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: omni_auth_services
+#
+#  id                  :bigint           not null, primary key
+#  access_token        :string
+#  access_token_secret :string
+#  auth                :text
+#  expires_at          :datetime
+#  provider            :string
+#  refresh_token       :string
+#  uid                 :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  user_id             :bigint           not null
+#
+# Indexes
+#
+#  index_omni_auth_services_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
 class OmniAuthService < ApplicationRecord
   belongs_to :user
 end
