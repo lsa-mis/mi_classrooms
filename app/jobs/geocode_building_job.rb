ab26b4f7dbcb580e04b49f44da2b665c24ccefac
David Chmura <dschmura@humbledaisy.com>
2020-10-13 16:15:13 -0400

    UPDATE: pass in the building for updating the geolocation

--- app/jobs/geocode_building_job.rb
+++ app/jobs/geocode_building_job.rb
@@ -1,9 +1,8 @@
 class GeocodeBuildingJob < ApplicationJob
-  sidekiq_options retry: 5
+
   queue_as :default
 
-  def perform(bldrecnbr)
-    building = Building.find(bldrecnbr)
+  def perform(building)
     address = [building.address, building.city, building.zip].join(" ")
     latitude, longitude = Geocoder.coordinates(address)
     building.update!(latitude: latitude, longitude: longitude)
