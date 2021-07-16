c207a6de5519145a26e6e238fd1fb6b3cf81828a
Dave Chmura <dschmura@humbledaisy.com>
2021-07-16 14:46:29 -0400

    UPDATES: Flashy new flashes

--- app/packs/controllers/index.js
+++ app/packs/controllers/index.js
@@ -8,7 +8,8 @@ const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
 
-import { Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
 application.register('autosave', Autosave)
 application.register('dropdown', Dropdown)
 application.register('modal', Modal)
