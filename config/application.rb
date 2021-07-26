61a6477605f2a56f37dea32a5555d3f232849feb
Dave Chmura <dschmura@humbledaisy.com>
2021-07-26 09:22:39 -0400

    UPDATE: config.load_defaults to 6.1 from 6.0 to fix rails/ujs

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module MiClassrooms
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
     config.generators do |g|
       g.test_framework false
       g.stylesheets false
