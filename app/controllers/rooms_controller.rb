0075905810f7031d17522cd0cdf61034058a1de3
Dave Chmura <dschmura@humbledaisy.com>
2021-08-31 13:12:46 -0400

    UPDATE: enable dual range slider for student capacity filtering

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,11 +5,17 @@ include ActionView::RecordIdentifier
   # GET /rooms
   # GET /rooms.json
   def index
-    @rooms = Room.classrooms.includes([:building, :room_contact])
+
+    @rooms = Room.classrooms.includes([:building, :room_contact]).where('instructional_seating_count > ?', 1) 
 
     @rooms = @rooms.classrooms.with_building_name(params[:query]) if params[:query].present?
-
+    @rooms = @rooms.classrooms.with_school_or_college_name(params[:school_or_college_name]) if params[:school_or_college_name].present?
     @rooms = @rooms.classrooms.with_all_characteristics(params[:room_characteristics]) if params[:room_characteristics].present?
+    @rooms = @rooms.where('instructional_seating_count > ?', params[:min_capacity].to_i) 
+    @rooms = @rooms.where('instructional_seating_count > ?', capacity_slider_maximum) if params[:capacity_slider_maximum].present?
+
+    # school_or_college_name
+    @rooms = @rooms.with_school_or_college_name(params[:school_or_college_name]) if params[:school_or_college_name].present?  
 
     @rooms = RoomDecorator.decorate_collection(@rooms)
     @pagy, @rooms = pagy(@rooms)
@@ -87,19 +93,17 @@ include ActionView::RecordIdentifier
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_room
-      @room = Room.find(params[:id])
+      @room = Room.includes([:building]).find(params[:id])
     end
 
+    
     # Only allow a list of trusted parameters through.
     def room_params
-      params.require(:room).permit(:rmrecnbr, :floor, :room_number, :rmtyp_description, :dept_id, :dept_grp, :dept_description, :square_feet, :instructional_seating_count, :visible, :building_bldrecnbr, :room_characteristics)
+      params.require(:room).permit(:rmrecnbr, :floor, :room_number, :rmtyp_description, :dept_id, :dept_grp, :dept_description, :square_feet, :instructional_seating_count, :visible, :building_bldrecnbr, :room_characteristics, :min_capacity, :max_capacity, :school_or_college_name)
     end
 
     def filtering_params
-      params.slice(:bluray, :chalkboard, :doccam, :interactive_screen, :instructor_computer, :lecture_capture, :projector_16mm, :projector_35mm, :projector_digital_cinema, :projector_digial, :projector_slide, :team_board, :team_tables, :team_technology, :vcr, :video_conf, :whiteboard).values
+      params.slice(:bluray, :chalkboard, :doccam, :interactive_screen, :instructor_computer, :lecture_capture, :projector_16mm, :projector_35mm, :projector_digital_cinema, :projector_digial, :projector_slide, :team_board, :team_tables, :team_technology, :vcr, :video_conf, :whiteboard)
     end
 
-    def filtering_params
-      params.slice(:filter_params)
-    end
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -9,23 +9,23 @@ module SearchHelper
 
 
   def capacity_slider_minimum
-    if params[:query]
-      # if params[:query][:instructional_seating_count_gteq]
-      #   params[:query][:instructional_seating_count_gteq]
-      # else
-      #   1
-      # end
+    if params[:min_capacity]
+      if params[:min_capacity]
+        params[:min_capacity]
+      else
+        1
+      end
       else
       1
     end
   end
   def capacity_slider_maximum
     if params[:query]
-      # if params[:query][:instructional_seating_count_lteq]
-      #   params[:query][:instructional_seating_count_lteq]
-      # else
-      #   600
-      # end
+      if params[:max_capacity]
+        params[:max_capacity]
+      else
+        600
+      end
     else
       600
     end
