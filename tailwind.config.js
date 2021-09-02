86f45f7ff0bb15f1ed220d0834b5f1688797350c
Dave Chmura <dschmura@humbledaisy.com>
2021-09-02 16:12:33 -0400

    ADD: Support for tailwindcss/aspect-ration styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -107,6 +107,6 @@ module.exports = {
   plugins: [
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
-    // require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
