ae29b78be7daaac4d3cf85fed7e596c5956bee48
Margarita Barvinok <brita@umich.edu>
2021-09-20 11:14:29 -0400

    Search with autosubmit controller

--- app/packs/controllers/autosubmit_controller.js
+++ app/packs/controllers/autosubmit_controller.js
@@ -1,47 +1,14 @@
 import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['form', 'status']
-	static values = { duration: String }
 
-  connect() {
-    this.timeout = null
-    this.duration = fetch(this.durationValue) || 400
-
-  }
-
-	
-  save() {
+  search() {
     clearTimeout(this.timeout)
-		
+
     this.timeout = setTimeout(() => {
       this.statusTarget.textContent = 'Updating...'
-      // this.formTarget.submit()
-			this.element.requestSubmit()
-    }, this.duration)
+      this.element.requestSubmit()
+    }, 600)
   }
 
-  change(event){
-    event.preventDefault()
-    this.save()
-
-  }
-
-  success() {
-    this.timeout = setTimeout(() => {
-      this.statusTarget.textContent = 'Updated'
-    }, this.duration)
-
-  }
-
-  error() {
-    this.setStatus('Unable to update!')
-  }
-
-  setStatus(message) {
-    this.statusTarget.textContent = message
-
-    this.timeout = setTimeout(() => {
-      this.statusTarget.textContent = ''
-    }, 2000)
-  }
 }
