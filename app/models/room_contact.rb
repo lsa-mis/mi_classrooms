a220c579b2d80506db3088f867d9014f0ff93165
Margarita Barvinok <brita@umich.edu>
2021-10-13 23:31:59 -0400

    Remove dependent: :destroy from room_cotacts

--- app/models/room_contact.rb
+++ app/models/room_contact.rb
@@ -1,5 +1,5 @@
 class RoomContact < ApplicationRecord
-  belongs_to :room, foreign_key: :rmrecnbr, dependent: :destroy
+  belongs_to :room, foreign_key: :rmrecnbr
 
   validates_presence_of :rmrecnbr
 end
