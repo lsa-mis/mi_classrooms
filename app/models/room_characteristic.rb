9a79161c4cf36cc18ab9a93eba5cade79d69741e
Margarita Barvinok <brita@umich.edu>
2021-10-19 07:50:00 -0400

    Fix relations between rooms table and its dependent tables

--- app/models/room_characteristic.rb
+++ app/models/room_characteristic.rb
@@ -21,7 +21,7 @@
 #  fk_rails_...  (rmrecnbr => rooms.rmrecnbr)
 #
 class RoomCharacteristic < ApplicationRecord
-  belongs_to :room, foreign_key: :rmrecnbr, dependent: :destroy
+  belongs_to :room, foreign_key: :rmrecnbr
 
   validates_presence_of :rmrecnbr
 
