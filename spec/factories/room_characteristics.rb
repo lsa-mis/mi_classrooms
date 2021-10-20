38ad38a8ddad3827f517402121fbeb766cd6ef5a
Margarita Barvinok <brita@umich.edu>
2021-10-20 15:09:15 -0400

    Add factories

--- spec/factories/room_characteristics.rb
+++ spec/factories/room_characteristics.rb
@@ -1,5 +1,34 @@
+== Schema Information
+#
+# Table name: room_characteristics
+#
+#  id                :bigint           not null, primary key
+#  chrstc            :integer
+#  chrstc_desc254    :string
+#  chrstc_descr      :string
+#  chrstc_descrshort :string
+#  chrstc_eff_status :integer
+#  rmrecnbr          :bigint           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_room_characteristics_on_rmrecnbr  (rmrecnbr)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (rmrecnbr => rooms.rmrecnbr)
+#
+
 FactoryBot.define do
   factory :room_characteristic do
+    chrstc            { Faker::Number.number(digits: 3) }
+    chrstc_desc254    { Faker::String.random(length: 6..60) }
+    chrstc_descr      { Faker::String.random(length: 6..22) }
+    chrstc_descrshort { Faker::String.random(length: 6..12) }
+    chrstc_eff_status { Faker::Number.number }
+    rmrecnbr          { Faker::Number.number(digits: 7) }
     
   end
 end
--- spec/factories/room_contacts.rb
+++ spec/factories/room_contacts.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :room_contact do
+    rmrecnbr             { Faker::Number.number(digits: 7) }
+    rm_schd_cntct_name   { Faker::String.random(length: 6..60) }
+    rm_schd_email        { Faker::Internet.email }
+    rm_schd_cntct_phone  { Faker::PhoneNumber.phone_number }
+    rm_det_url           { Faker::Internet.url }
+    rm_usage_guidlns_url { Faker::Internet.url }
+    rm_sppt_deptid       { Faker::String.random(length: 6..10) }
+    rm_sppt_dept_descr   { Faker::String.random(length: 6..60) }
+    rm_sppt_cntct_email  { Faker::Internet.email }
+    rm_sppt_cntct_phone  { Faker::PhoneNumber.phone_number }
+    rm_sppt_cntct_url    { Faker::Internet.url }
     
   end
 end
