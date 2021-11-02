f9f168874274f3ebe3cf03b76bc7ed7e1f44d9db
Margarita Barvinok <brita@umich.edu>
2021-11-02 08:34:25 -0400

    Include department api

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require_relative "application"
 require "./lib/auth_token_api"
 require "./lib/buildings_api"
 require "./lib/classroom_api"
+require "./lib/department_api"
 
 # Initialize the Rails application.
 Rails.application.initialize!
