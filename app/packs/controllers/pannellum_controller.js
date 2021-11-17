3e7cbb7d126c29c2bd3347ea5eb103e3ae74ad7e
Rick Smoke <rsmoke@umich.edu>
2021-11-17 10:58:23 -0500

    update to doisplay a panoramic image

--- app/packs/controllers/pannellum_controller.js
+++ app/packs/controllers/pannellum_controller.js
@@ -1,21 +1,23 @@
-import { Controller } from "stimulus"
-require('pannellum')
-import 'pannellum/src/css/pannellum.css';
+// import { Controller } from "stimulus"
+// require('pannellum')
+// import 'pannellum/src/css/pannellum.css';
 
-export default class extends Controller {
-  static targets = [ "panorama" ]
+// export default class extends Controller {
+//   static targets = [ "panorama" ]
 
-  connect(){
-    let panoImage = this.data.get("panoimage")
-    let panoPreview = this.data.get("panopreview")
-    this.pano(panoImage, panoPreview)
-  }
-  pano(panoImage, panoPreview) {
+//   connect(){
+//     console.log("In Connect")
+//     console.log(this.data)
+//     let panoImage = this.data.get("panoimage")
+//     let panoPreview = this.data.get("panopreview")
+//     this.pano(panoImage, panoPreview)
+//   }
+//   pano(panoImage, panoPreview) {
 
-    pannellum.viewer('panorama', {
-      "type": "equirectangular",
-      "panorama": panoImage,
-      "preview": panoPreview
-    });
-  }
-}
+//     pannellum.viewer('panorama', {
+//       "type": "equirectangular",
+//       "panorama": panoImage,
+//       "preview": panoPreview
+//     });
+//   }
+// }
