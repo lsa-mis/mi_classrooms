95f88ce37603d7823ff4fb9cba74abb4d1315db9
Margarita Barvinok <brita@umich.edu>
2021-11-19 10:32:31 -0500

    Include additional building into updating tasks

--- lib/buildings_api.rb
+++ lib/buildings_api.rb
@@ -88,14 +88,14 @@ class BuildingsApi
 
   # update builgings
 
-  def update_all_buildings(campus_codes = [100])
+  def update_all_buildings(campus_codes = [100], buildinds_codes = [])
     @buildings_ids = Building.all.pluck(:bldrecnbr)
 
     @result = get_buildings_for_current_fiscal_year
     if @result['success']
       data = @result['data']
       data.each do |row|
-        if campus_codes.include?(row['BuildingCampusCode'])
+        if campus_codes.include?(row['BuildingCampusCode']) || buildinds_codes.include?(row['BuildingRecordNumber'])
           if building_exists?(row['BuildingRecordNumber'])
             update_building(row)
           else
