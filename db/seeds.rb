718c37e6d30e3c120992ec84d7b1712984996f99
rsmokeUM <rsmoke@umich.edu>
2021-11-22 17:07:53 -0500

    fixed typo
    existion => exisiting

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 locations = ['index_page', 'rooms_page']
-existion_locations = Announcement.all.pluck(:location)
+existing_locations = Announcement.all.pluck(:location)
 
 locations.each do |location|
-  unless existion_locations.include?(location)
+  unless existing_locations.include?(location)
     Announcement.create!(location: location)
   end
 end
