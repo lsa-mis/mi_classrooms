5816c50265ea0f5778e2c45d84f7ba65de660736
Rick Smoke <rsmoke@umich.edu>
2021-11-23 16:18:28 -0500

    helpers to show room image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,10 +18,26 @@ module ApplicationHelper
   end
 
   def building_image( room )
-    if room.building.building_image.attached?
-      image_tag room.building.building_image.representation(resize_to_limit: [150, 150]), class: 'mr-2', alt: "#{room.room_number} --  #{room.building.name}" 
+    if room.building.building_image.representable?
+      image_tag room.building.building_image.representation(resize_to_limit: [150, 150]), class: 'm-2', alt: "#{room.room_number} --  #{room.building.name}" 
     else
       image_tag 'building_placeholder', height: '150', width: '150', class: 'mr-2', alt: "building placeholder" 
+    end
+  end
+
+  def room_image( room )
+    if room.room_image.representable?
+      image_tag room.room_image.representation(resize_to_limit: [600, 400]), class: 'm-2', alt: "#{room.room_number} --  #{room.building.name}" 
+    else
+      image_tag 'room_placeholder', height: '150', width: '150', class: 'mr-2', alt: "room placeholder" 
+    end
+  end
+
+  def room_thumbnail_image( room )
+    if room.room_image.representable?
+      image_tag room.room_image.representation(resize_to_limit: [100, 80]), class: 'p-2', alt: "#{room.room_number} --  #{room.building.name}" 
+    else
+      image_tag 'room_placeholder', height: '75', width: '100', class: 'p-2', alt: "room placeholder" 
     end
   end
 
