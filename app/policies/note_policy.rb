8bf0443456b537e9d3b0de2d0aab59590190dbf1
Margarita Barvinok <brita@umich.edu>
2021-11-30 09:11:34 -0500

    Add policy for notes

---
+++ app/policies/note_policy.rb
@@ -0,0 +1,28 @@
+class NotePolicy < ApplicationPolicy
+  
+
+  def index?
+    # true
+    user_in_non_admin_group?
+  end
+
+  def show?
+    # true
+    user_in_non_admin_group?
+  end
+
+  def create?
+    user.admin
+  end
+  # def toggle_visibility?
+  #   user && user_in_admin_group?
+  # end
+
+  def edit?
+    user.admin
+  end
+
+  def update?
+    edit?
+  end
+end
