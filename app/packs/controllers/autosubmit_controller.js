c4534bfcee0c73a7079582512ba0ab9025bd3180
Margarita Barvinok <brita@umich.edu>
2021-12-02 17:09:10 -0500

    temp

--- app/packs/controllers/autosubmit_controller.js
+++ app/packs/controllers/autosubmit_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from 'stimulus'
 export default class extends Controller {
-  static targets = ['form', 'status', 'sidebar']
+  static targets = ['form', 'status', 'sidebar', 'min_capacity', 'max-capacity', 'capacity_error']
 
   search() {
     clearTimeout(this.timeout)
@@ -13,6 +13,28 @@ export default class extends Controller {
   }
 
   checkboxSubmit() {
+    clearTimeout(this.timeout)
+
+    this.timeout = setTimeout(() => {
+      this.statusTarget.textContent = 'Updating...'
+      Turbo.navigator.submitForm(this.formTarget)
+      this.sidebarTarget.classList.toggle('-translate-x-full')
+    }, 0)
+  }
+
+  capacitySubmit() {
+    var min_capacity = this.min_capacityTarget
+    var max_capacity = this.max_capacityTarget
+
+    if (min_capacity > max_capacity) {
+      this.capacity_errorTarget.classList.add("device-error--display")
+      this.capacity_errorTarget.classList.remove("device-error--hide")
+    }
+    else {
+      this.capacity_errorTarget.classList.add("device-error--hide")
+      this.capacity_errorTarget.classList.remove("device-error--display")
+    }
+
     clearTimeout(this.timeout)
 
     this.timeout = setTimeout(() => {
