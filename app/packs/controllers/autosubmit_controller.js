34d5c6b7412016c02cb099109c661fed16d25d0c
Margarita Barvinok <brita@umich.edu>
2021-12-03 10:55:52 -0500

    remove unneeded code

--- app/packs/controllers/autosubmit_controller.js
+++ app/packs/controllers/autosubmit_controller.js
@@ -28,12 +28,10 @@ export default class extends Controller {
     this.timeout = setTimeout(() => {
       var min_capacity = parseInt(this.min_capacityTarget.value)
       var max_capacity = parseInt(this.max_capacityTarget.value)
-
       if (min_capacity > max_capacity) {
         this.capacity_errorTarget.classList.add("capacity-error--display")
         this.capacity_errorTarget.classList.remove("capacity-error--hide")
         this.capacity_errorTarget.innerText = "Min should be smaller than Max"
-        event.preventDefault()
       }
       else {
         this.capacity_errorTarget.classList.add("capacity-error--hide")
