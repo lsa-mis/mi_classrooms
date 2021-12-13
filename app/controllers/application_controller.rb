fed8cd6f4035302db7173001fcbad9b3476153f3
Margarita Barvinok <brita@umich.edu>
2021-12-13 15:34:25 -0500

    Remove unneeded methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   include Pundit
-  # before_action :store_user_location!, if: :storable_location?
-  # before_action :create_feedback
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_in_group
   before_action :set_membership
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
 
   private
-
-  # def sign_up_params
-  #   params.require(:user).permit(:uniqname, :email, :password, :password_confirmation)
-  # end
-
-  # def account_update_params
-  #   params.require(:user).permit(:uniqname, :email, :password, :password_confirmation, :current_password)
-  # end
-
 
   def user_not_authorized
     flash[:alert] = "Please sign in to perform this action."
@@ -28,19 +18,6 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to about_path
   end
-
-  # def storable_location?
-  #   request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
-  # end
-
-
-  # def store_user_location!
-  #   store_location_for(:user, request.fullpath)
-  # end
-
-  # def create_feedback
-  #   @feedback = Feedback.new
-  # end
 
   def set_membership
     if user_signed_in?
