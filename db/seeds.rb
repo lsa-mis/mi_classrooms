4deaa4717e0dc22980bf32ce9a27e22af85051de
Margarita Barvinok <brita@umich.edu>
2021-12-17 09:18:01 -0500

    Keep db updates there were run manually

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,59 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-locations = ['index_page', 'rooms_page']
-existing_locations = Announcement.all.pluck(:location)
+# The following was run on the staging server manualy
+# Put here to keep records of these updates
 
-locations.each do |location|
-  unless existing_locations.include?(location)
-    Announcement.create!(location: location)
-  end
-end
+# locations = ['index_page', 'rooms_page']
+# existing_locations = Announcement.all.pluck(:location)
+
+# locations.each do |location|
+#   unless existing_locations.include?(location)
+#     Announcement.create!(location: location)
+#   end
+# end
+
+# Building.find_by(bldrecnbr: 1000151).update(nick_name: "UMMA")
+# Building.find_by(bldrecnbr: 1005046).update(nick_name: "USB")
+# Building.find_by(bldrecnbr: 1000165).update(nick_name: "WEIS")
+# Building.find_by(bldrecnbr: 1000162).update(nick_name: "DENT")
+# Building.find_by(bldrecnbr: 1005235).update(nick_name: "LSSH")
+# Building.find_by(bldrecnbr: 1005177).update(nick_name: "NQ")
+# Building.find_by(bldrecnbr: 1000440).update(nick_name: "SM")
+# Building.find_by(bldrecnbr: 1000054).update(nick_name: "EQ")
+# Building.find_by(bldrecnbr: 1000234).update(nick_name: "SPH")
+# Building.find_by(bldrecnbr: 1000333).update(nick_name: "400NI")
+# Building.find_by(bldrecnbr: 1005101).update(nick_name: "WEILL")
+# Building.find_by(bldrecnbr: 1005370).update(nick_name: "BLAU")
+# Building.find_by(bldrecnbr: 1000211).update(nick_name: "SKB")
+# Building.find_by(bldrecnbr: 1000216).update(nick_name: "TAP")
+# Building.find_by(bldrecnbr: 1000207).update(nick_name: "MLB")
+# Building.find_by(bldrecnbr: 1005451).update(nick_name: "CCCB")
+# Building.find_by(bldrecnbr: 1005188).update(nick_name: "R-BUS")
+# Building.find_by(bldrecnbr: 1000158).update(nick_name: "CHEM")
+# Building.find_by(bldrecnbr: 1000152).update(nick_name: "AH")
+# Building.find_by(bldrecnbr: 1000154).update(nick_name: "LORCH")
+# Building.find_by(bldrecnbr: 1000059).update(nick_name: "ALH")
+# Building.find_by(bldrecnbr: 1000179).update(nick_name: "HUTCH")
+# Building.find_by(bldrecnbr: 1000197).update(nick_name: "MH")
+# Building.find_by(bldrecnbr: 1000188).update(nick_name: "NUB")
+# Building.find_by(bldrecnbr: 1000221).update(nick_name: "SEB")
+# Building.find_by(bldrecnbr: 1000206).update(nick_name: "AH")
+# Building.find_by(bldrecnbr: 1000204).update(nick_name: "SPH")
+# Building.find_by(bldrecnbr: 1005059).update(nick_name: "WDC")
+# Building.find_by(bldrecnbr: 1005347).update(nick_name: "426NI")
+# Building.find_by(bldrecnbr: 1000184).update(nick_name: "LLIBS")
+# Building.find_by(bldrecnbr: 1005179).update(nick_name: "STB")
+# Building.find_by(bldrecnbr: 1000219).update(nick_name: "SSWB")
+# Building.find_by(bldrecnbr: 1000150).update(nick_name: "LSA")
+# Building.find_by(bldrecnbr: 1000189).update(nick_name: "DANA")
+# Building.find_by(bldrecnbr: 1000175).update(nick_name: "HH")
+# Building.find_by(bldrecnbr: 1000166).update(nick_name: "EH")
+# Building.find_by(bldrecnbr: 1000890).update(nick_name: "PERRY")
+# Building.find_by(bldrecnbr: 1005224).update(nick_name: "STAMPS")
+# Building.find_by(bldrecnbr: 1000155).update(nick_name: "BMT")
+# Building.find_by(bldrecnbr: 1000167).update(nick_name: "WH")
+# Building.find_by(bldrecnbr: 1005169).update(nick_name: "BSB")
+# Building.find_by(bldrecnbr: 1005047).update(nick_name: "PALM")
+# Building.find_by(bldrecnbr: 1000208).update(nick_name: "RAND")
+
