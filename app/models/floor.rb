23b4e6fa925e49a06e21bd18a6aa1b60cfa5ed11
Margarita Barvinok <brita@umich.edu>
2022-01-12 17:46:26 -0500

    add image validation

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -3,6 +3,19 @@ class Floor < ApplicationRecord
   has_one_attached :floor_plan
 
   validates_uniqueness_of :floor, :scope => :building_bldrecnbr, :message => '- combination of floor and building_bldrecnbr should be unique'
+  validate :acceptable_floor_plan
 
+  def acceptable_floor_plan
+    return unless floor_plan.attached?
 
+    acceptable_types = ["image/jpg", "image/jpeg", "image/png"]
+
+    unless floor_plan.byte_size <= 20.megabyte
+      errors.add(:floor_plan, "is too big")
+    end
+
+    unless acceptable_types.include?(floor_plan.content_type)
+      errors.add(:floor_plan, "must be an image")
+    end
+  end
 end
