ff101f1f3b4df61fd3e976736ee047d9430495d2
Margarita Barvinok <brita@umich.edu>
2022-01-12 16:45:59 -0500

    validate uniqueness of building + floor

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -1,4 +1,8 @@
 class Floor < ApplicationRecord
   belongs_to :building, foreign_key: :building_bldrecnbr
   has_one_attached :floor_plan
+
+  validates_uniqueness_of :floor, :scope => :building_bldrecnbr, :message => '- combination of floor and building_bldrecnbr should be unique'
+
+
 end
