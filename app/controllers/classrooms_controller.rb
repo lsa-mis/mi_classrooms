c1e40adf0486ca4cd944e7aa3a505f626c27ec6a
Rick Smoke <rsmoke@umich.edu>
2022-02-23 13:51:20 -0500

    added outdated link alert message

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -4,7 +4,7 @@ class ClassroomsController < ApplicationController
   def index
     skip_policy_scope
 
-    redirect_to rooms_path + "?school_or_college_name=College+of+Lit%2C+Science+%26+Arts"
+    redirect_to rooms_path + "?school_or_college_name=College+of+Lit%2C+Science+%26+Arts", alert: "You arrived here from an outdated link"
   end
 
   def show
@@ -12,6 +12,6 @@ class ClassroomsController < ApplicationController
 
     uri_substring = URI.split(request.url)[5]
     room_from_url = uri_substring.split('/')[2]
-    redirect_to room_path(Room.find_by(facility_code_heprod: room_from_url))
+    redirect_to room_path(Room.find_by(facility_code_heprod: room_from_url)), alert: "You arrived here from an outdated link"
   end
 end
