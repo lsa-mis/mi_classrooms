49a881ff5ef94a90a0323e52a396d7f57f1dfa0c
Margarita Barvinok <brita@umich.edu>
2022-03-31 14:56:26 -0400

    Add/edit/remove and display alerts

--- app/models/note.rb
+++ app/models/note.rb
@@ -10,11 +10,28 @@ class Note < ApplicationRecord
 
   validates :body, presence: true
 
+  scope :alert, -> { where(alert: true) }
+  scope :notice, -> { where(alert: false) }
+
   after_create_commit do
-    broadcast_append_to [noteable, :notes], target: "#{dom_id(noteable)}_notes"
+    if self.alert
+      broadcast_append_to [noteable, :alerts], target: "#{dom_id(noteable)}_alerts"
+    else
+      broadcast_append_to [noteable, :notes], target: "#{dom_id(noteable)}_notes"
+    end
   end
   after_update_commit do
-    broadcast_replace_to self
+    if self.previous_changes.has_key?('alert')
+      if self.alert
+        broadcast_remove_to self
+        broadcast_append_to [noteable, :alerts], target: "#{dom_id(noteable)}_alerts"
+      else
+        broadcast_remove_to self
+        broadcast_append_to [noteable, :notes], target: "#{dom_id(noteable)}_notes"
+      end
+    else
+      broadcast_replace_to self
+    end
   end
   after_destroy_commit do
     broadcast_remove_to self
