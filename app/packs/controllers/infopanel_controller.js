67188f2f46774a4b0edf1381e2ca59cea90fd604
Rick Smoke <rsmoke@umich.edu>
2022-04-16 19:07:08 -0400

    add collapsable information panel

---
+++ app/packs/controllers/infopanel_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus"
+export default class InfoPanelController extends Controller {
+
+  static targets = ["info_text_area", "info_text_short_area"]
+
+  toggle() {
+    // console.log ("toggle you are in the money")
+    this.info_text_areaTarget.classList.add("infopanel--display")
+    this.info_text_areaTarget.classList.remove("infopanel--hide")
+    this.info_text_short_areaTarget.classList.add("infopanel--hide")
+    this.info_text_short_areaTarget.classList.remove("infopanel--display")
+  }
+
+  toggle2() {
+    // console.log ("toggle2 you are in the money")
+    this.info_text_areaTarget.classList.add("infopanel--hide")
+    this.info_text_areaTarget.classList.remove("infopanel--display")
+    this.info_text_short_areaTarget.classList.add("infopanel--display")
+    this.info_text_short_areaTarget.classList.remove("infopanel--hide")
+  }
+}
