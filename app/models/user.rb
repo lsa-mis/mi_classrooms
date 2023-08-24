4ff8346c00ab8c637142ff21016059283e8f9edf
Margarita Barvinok <56089936+britaumich@users.noreply.github.com>
2023-08-24 15:27:17 -0400

    Solution for error No route matches [GET] (#292)
    * remove registerable from devise
    
    * elegant error for No route matches [GET]
    
    * remove recoverable from User model
    
    * remove not_found method
    
    * remove validatable
    
    * remove views/devise folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:saml]
+  devise :database_authenticatable,
+         :rememberable, :omniauthable, omniauth_providers: [:saml]
 
   attr_accessor :membership, :admin
   has_many :omni_auth_services, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -41,5 +41,5 @@ Rails.application.routes.draw do
   end
 
   get 'application/delete_file_attachment/:id', to: 'application#delete_file_attachment', as: :delete_file
-  
+
 end
