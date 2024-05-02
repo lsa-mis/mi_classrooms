65708237a0e20fabca49b8afdb51f2d5c2dc9fae
rsmokeUM <rsmoke@umich.edu>
2024-05-02 09:59:39 -0400

    Refactor AuthTokenApi class to use frozen string literal and update method names

--- lib/auth_token_api.rb
+++ lib/auth_token_api.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'uri'
 require 'json'
 require 'openssl'
 
+# The AuthTokenApi class is designed to interact with an OAuth2 token provider API
+# to obtain authentication tokens. This class encapsulates the necessary steps to
+# request, receive, and handle responses for authentication tokens based on given scopes.
 class AuthTokenApi
-  BASE_URL = "https://gw.api.it.umich.edu/um/oauth2/token"
+  BASE_URL = 'https://gw.api.it.umich.edu/um/oauth2/token'
 
   def initialize(scope)
     @scope = scope
     @logger = ApiLog.instance.logger
   end
 
-  def get_auth_token
+  def obtain_auth_token
     response_json = request_auth_token
-
-    # puts "AuthTokenApi::get_auth_token #{response_json }"
 
     if response_json['access_token']
       success_response(response_json['access_token'])
@@ -35,8 +38,8 @@ class AuthTokenApi
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Post.new(uri)
-    request["content-type"] = 'application/x-www-form-urlencoded'
-    request["accept"] = 'application/json'
+    request['content-type'] = 'application/x-www-form-urlencoded'
+    request['accept'] = 'application/json'
     request.body = build_request_body
 
     response = http.request(request)
@@ -49,11 +52,19 @@ class AuthTokenApi
   end
 
   def success_response(access_token)
-    {'success' => true, 'error' => '', 'access_token' => access_token}
+    {
+      'success' => true,
+      'error' => '',
+      'access_token' => access_token
+    }
   end
 
   def error_response(error)
-    {'success' => false, 'error' => error, 'access_token' => nil}
+    {
+      'success' => false,
+      'error' => error,
+      'access_token' => nil
+    }
   end
 
   def log_error(message)
@@ -62,6 +73,10 @@ class AuthTokenApi
   end
 end
 
+# Singleton ApiLog: By including the Singleton module, ApiLog ensures that logger is initialized only once and reused,
+# enhancing performance.
+# Rails Configuration: Storing the logger file path in Rails.application.config allows easy changes and
+#  environment-specific configurations without altering the service class.
 class ApiLog
   include Singleton
 
@@ -70,17 +85,20 @@ class ApiLog
   end
 end
 
-# app/models/task_result_log.rb
+# The TaskResultLog class is responsible for logging the results of API operations.
+# It provides functionality to log both success and error statuses based on the execution
+# of API operations. It leverages a singleton logger for logging errors directly
+# related to the logging process itself.
 class TaskResultLog
   def initialize
     @logger = ApiLog.instance.logger
   end
 
   def update_log(message, debug)
-    status = debug ? "error" : "success"
-    unless ApiUpdateLog.create(result: message, status: status)
-      log_error(message)
-    end
+    status = debug ? 'error' : 'success'
+    return if ApiUpdateLog.create(result: message, status:)
+
+    log_error(message)
   end
 
   private
@@ -89,9 +107,3 @@ class TaskResultLog
     @logger.debug "API Update Log Error: Could not save - #{message}"
   end
 end
-
-# Singleton ApiLog: By including the Singleton module, ApiLog ensures that logger is initialized only once and reused, enhancing performance.
-
-# Rails Configuration: Storing the logger file path in Rails.application.config allows easy changes and environment-specific configurations without altering the service class.
-
-# Improved Error Handling: The update_log method now uses a more Ruby idiomatic approach, creating the ApiUpdateLog record and logging to file only if it fails to save.
