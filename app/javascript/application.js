8ff9da1f00b0701d4849fe94e3fa89430cecfdca
Dave Chmura <dschmura@humbledaisy.com>
2024-11-12 09:30:31 -0500

    ADD: Actiontext

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
+import "trix"
+import "@rails/actiontext"
+import * as ActiveStorage from "@rails/activestorage"
+
+// Make Trix available globally
+window.Trix = Trix
+
+// Start ActiveStorage
+ActiveStorage.start()
---
+++ vendor/javascript/promise-polyfill.js
@@ -0,0 +1,3 @@
+// promise-polyfill@8.3.0 downloaded from https://ga.jspm.io/npm:promise-polyfill@8.3.0/src/index.js
+
+import"./finally.js";export{P as default}from"../_/da34a0a8.js";
