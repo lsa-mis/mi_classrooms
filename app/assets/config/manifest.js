b8071236fc4889fa14d1da19bf67d908acdfdf39
Dave Chmura <dschmura@humbledaisy.com>
2024-12-10 10:50:23 -0500

    UPDATE: Styles

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,11 +1,10 @@
-//= link_directory ../builds
+//= link_tree ../builds
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../javascripts .js
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 
-//= link tailwind.css
 //= link tailwindcss-stimulus-components.js
 //= link stimulus-flatpickr.js
 //= link flatpickr.js
