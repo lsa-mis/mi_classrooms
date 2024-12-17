0b4bbc122b3d0ef5bf0bb3aee25a163207f22ad7
Margarita Barvinok <brita@umich.edu>
2024-12-17 10:16:22 -0500

    add context to auth test

--- spec/models/auth_token_api_spec.rb
+++ spec/models/auth_token_api_spec.rb
@@ -2,18 +2,22 @@ require 'rails_helper'
 
 RSpec.describe AuthTokenApi, type: :model do
 
-  it "returns Invalid Scope error" do
-    auth_token_api = AuthTokenApi.new("buildings1")
-    auth_token = auth_token_api.get_auth_token
-    expect(auth_token).to be false
-    expect(ApiUpdateLog.last.status).to eq("error")
-    expect(ApiUpdateLog.last.result).to include("Invalid Scope")
+  context "the scope is not correct (hotdogs)" do
+    it "returns Invalid Scope error" do
+      auth_token_api = AuthTokenApi.new("hotdogs")
+      auth_token = auth_token_api.get_auth_token
+      expect(auth_token).to be false
+      expect(ApiUpdateLog.last.status).to eq("error")
+      expect(ApiUpdateLog.last.result).to include("Invalid Scope")
+    end
   end
 
-  it "returns valid token for 'buildings' scope" do
-    auth_token_api = AuthTokenApi.new("buildings")
-    auth_token = auth_token_api.get_auth_token
-    expect(auth_token).to be_a(String)
+  context "the scope is correct (buildings)" do
+    it "returns valid token for 'buildings' scope" do
+      auth_token_api = AuthTokenApi.new("buildings")
+      auth_token = auth_token_api.get_auth_token
+      expect(auth_token).to be_a(String)
+    end
   end
 
 end
