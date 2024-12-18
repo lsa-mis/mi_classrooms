7d36c581b966b71470ad6b54072e8fa66ed32b05
Margarita Barvinok <brita@umich.edu>
2024-12-18 10:50:37 -0500

    http_call failed test

--- spec/models/auth_token_api_spec.rb
+++ spec/models/auth_token_api_spec.rb
@@ -29,10 +29,24 @@ RSpec.describe AuthTokenApi, type: :model do
       allow(response).to receive(:read_body).and_return('{"token_type": "Bearer", "access_token": "2wq00IPhXzA7g6dSlh7qHzf2CgZD", "issued_at": 1734533939, "expires_in": 3599, "scope": "buildings", "client_id": "89z6gUkueEDqBQOAzemfpqSByTGJCBnlOGXtNpsG2bnQ1zFs"}')
       allow(auth_token_api).to receive(:http_call).and_return(response)
     end
-    
+
     it "returns valid token for 'buildings' scope" do
       auth_token = auth_token_api.get_auth_token
       expect(auth_token).to eq("2wq00IPhXzA7g6dSlh7qHzf2CgZD")
+    end
+  end
+
+  context "rescue error if http_call failed" do
+    let!(:auth_token_api) { AuthTokenApi.new("buildings") }
+
+    before do
+      allow(auth_token_api).to receive(:http_call).and_return("abc")
+    end
+    
+    it "logs the error" do
+      auth_token = auth_token_api.get_auth_token
+      expect(ApiUpdateLog.last.status).to eq("error")
+      expect(ApiUpdateLog.last.result).to include("undefined method `read_body' for an instance of String")
     end
   end
 
