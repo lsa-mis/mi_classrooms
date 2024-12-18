7c579fc2f0aaffcc33b6426f43dfd0db1b4aaf85
Margarita Barvinok <brita@umich.edu>
2024-12-18 10:16:49 -0500

    test passed

--- spec/models/auth_token_api_spec.rb
+++ spec/models/auth_token_api_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe AuthTokenApi, type: :model do
     it "returns Invalid Scope error" do
       auth_token_api = AuthTokenApi.new("hotdogs")
       response = double()
-      allow(response).to receive(:read_body).and_return("{}")
+      allow(response).to receive(:code).and_return("401")
+      allow(response).to receive(:read_body).and_return('{"fault": {"faultstring": "Invalid Scope", "detail": {"errorcode": "oauth.v2.InvalidScope"}}}')
       allow(auth_token_api).to receive(:http_call).and_return(response)
       auth_token = auth_token_api.get_auth_token
       expect(auth_token).to be false
