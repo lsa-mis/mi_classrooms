1fb5eb14fe631f0b97b004f226986cd90a2d0d48
Margarita Barvinok <brita@umich.edu>
2024-12-18 10:28:33 -0500

    make the test look better

--- spec/models/auth_token_api_spec.rb
+++ spec/models/auth_token_api_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe AuthTokenApi, type: :model do
 
   context "the scope is not correct (hotdogs)" do
-    it "returns Invalid Scope error" do
-      auth_token_api = AuthTokenApi.new("hotdogs")
+    let!(:auth_token_api) { AuthTokenApi.new("hotdogs") }
+
+    before do
       response = double()
       allow(response).to receive(:code).and_return("401")
       allow(response).to receive(:read_body).and_return('{"fault": {"faultstring": "Invalid Scope", "detail": {"errorcode": "oauth.v2.InvalidScope"}}}')
       allow(auth_token_api).to receive(:http_call).and_return(response)
+    end
+    
+    it "returns Invalid Scope error" do
       auth_token = auth_token_api.get_auth_token
       expect(auth_token).to be false
       expect(ApiUpdateLog.last.status).to eq("error")
