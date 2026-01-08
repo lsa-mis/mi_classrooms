5e9a90f44524e06790977705056aa374d840b1c7
Dave Chmura <dschmura@humbledaisy.com>
2026-01-08 10:47:12 -0500

    REFACTOR: Optimize notes factory to avoid unnecessary DB inserts
    Use build instead of create in after(:build) callback so that
    build(:note) no longer creates database records for building
    and room. Add after(:create) callback to persist noteable only
    when the note itself is being persisted.

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     alert { false }
 
     # Polymorphic association - defaults to room
+    # Pass noteable: to use an existing record instead of creating new ones
     transient do
       noteable { nil }
     end
@@ -13,9 +14,15 @@ FactoryBot.define do
       if evaluator.noteable
         note.noteable = evaluator.noteable
       else
-        building = create(:building, bldrecnbr: Faker::Number.unique.number(digits: 7))
-        note.noteable = create(:room, building_bldrecnbr: building.bldrecnbr, rmrecnbr: Faker::Number.unique.number(digits: 7))
+        # Use build to avoid DB inserts when using build(:note)
+        building = build(:building, bldrecnbr: Faker::Number.unique.number(digits: 7))
+        note.noteable = build(:room, building: building, building_bldrecnbr: building.bldrecnbr, rmrecnbr: Faker::Number.unique.number(digits: 7))
       end
+    end
+
+    after(:create) do |note|
+      # Ensure noteable is persisted when note is created
+      note.noteable.save! unless note.noteable.persisted?
     end
 
     trait :alert do
@@ -32,7 +39,7 @@ FactoryBot.define do
 
     trait :for_building do
       after(:build) do |note|
-        note.noteable = create(:building, bldrecnbr: Faker::Number.unique.number(digits: 7))
+        note.noteable = build(:building, bldrecnbr: Faker::Number.unique.number(digits: 7))
       end
     end
   end
